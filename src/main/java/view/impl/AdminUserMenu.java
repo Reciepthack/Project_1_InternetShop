66647098bfe76e90049ba81626b56877b6886987
Vladimir Shevchuck <67117750+Reciepthack@users.noreply.github.com>
2020-07-18 21:17:09 +0300

    for review

--- src/main/java/view/impl/AdminUserMenu.java
+++ src/main/java/view/impl/AdminUserMenu.java
@@ -15,7 +15,9 @@ public class AdminUserMenu implements Menu {
     public void show() {
         showItems(items);
         System.out.println("0. Exit");
-
+        userService.findAll();
+        userService.blockUser(1);
+        userService.unBlockUser(1);
         scanner = new Scanner(System.in);
     }
 
