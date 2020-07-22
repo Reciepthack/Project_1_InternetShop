ac454e44d14e35fb1160c03c112b1bcffea9028a
Shabatin-Anton <antonshabatin1993@gmail.com>
2020-07-22 21:22:52 +0300

    Add AdminProductsMenu(2)

--- src/main/java/view/impl/AdminProductsMenu.java
+++ src/main/java/view/impl/AdminProductsMenu.java
@@ -1,5 +1,6 @@
 package view.impl;
 
+import dao.ProductDaoImpl;
 import model.Product;
 import service.ProductService;
 import service.ProductServiceImpl;
@@ -7,16 +8,18 @@ import view.Menu;
 
 import javax.print.DocFlavor;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AdminProductsMenu implements Menu {
     private static final String Return = "0 Return to main menu";
     private static final String Incorrect = "Please try again";
+    private static final String[] Changes = {"Delete", "Update", "Save", Return};
     private Scanner scanner = new Scanner(System.in);
-    ProductService productService = ProductServiceImpl.getInstance();
+    private ProductService productService = ProductServiceImpl.getInstance();
     LoginMenu loginMenu = new LoginMenu();
 
-    private final String[] mainAdminProductMenu = {"1. Update Product",
+    private final String[] mainAdminProductMenu = {"1. Show all Products",
             "2. Add Product", "0. Exit"};
 
     @Override
@@ -31,10 +34,11 @@ public class AdminProductsMenu implements Menu {
             }
             switch (choice) {
                 case 1:
+                    showAllProducts(ProductDaoImpl);
                     break;
                 case 2:
                     break;
-                case 3:
+                case 0:
                     exit();
                     break;
             }
@@ -47,7 +51,49 @@ public class AdminProductsMenu implements Menu {
     }
 
     private void addNewProduct() {
+    }
 
+    private void updateProduct() {
+    }
+
+    private void saveProduct(){
+    }
+
+    private void deleteProduct(){
+    }
+
+    private void showAllProducts(Map<Long,Product> productMap){
+        for (Map.Entry entry: productMap.entrySet()) {
+            System.out.println("ID: " + entry.getKey() + "Product: " + entry.getValue());
+        }
+        System.out.println(Changes);
+        subMenuDeleteUpdateSave();
+
+    }
+
+    private void subMenuDeleteUpdateSave(){
+        int choice = 0;
+        while (true) {
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println(Incorrect);
+            }
+            switch (choice) {
+                case 1:
+                    deleteProduct();
+                    break;
+                case 2:
+                    updateProduct();
+                    break;
+                case 3:
+                    saveProduct();
+                    break;
+                case 0:
+                    exit();
+                    break;
+            }
+        }
     }
 
     private void findProductById() {
